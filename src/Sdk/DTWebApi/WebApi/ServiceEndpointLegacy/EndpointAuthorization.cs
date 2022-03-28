f8abae5d6567b1c21264c928d68b127860b0362e
jonatanlr98 <78903430+jonatanlr98@users.noreply.github.com>
2022-03-28 02:19:15 -0700

    Initialize users/juliobbv/runnerAuthDoc

---
+++ src/Sdk/DTWebApi/WebApi/ServiceEndpointLegacy/EndpointAuthorization.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace GitHub.DistributedTask.WebApi
+{
+    public sealed class EndpointAuthorizationSchemes
+    {
+        public const String OAuth = "OAuth";
+    }
+
+    public sealed class EndpointAuthorizationParameters
+    {
+        public const String AccessToken = "AccessToken";
+    }
+
+    [DataContract]
+    public sealed class EndpointAuthorization
+    {
+        public EndpointAuthorization()
+        {
+        }
+
+        private EndpointAuthorization(EndpointAuthorization authorizationToClone)
+        {
+            this.Scheme = authorizationToClone.Scheme;
+            if (authorizationToClone.m_parameters != null && authorizationToClone.m_parameters.Count > 0)
+            {
+                m_parameters = new Dictionary<String, String>(authorizationToClone.m_parameters, StringComparer.OrdinalIgnoreCase);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the scheme used for service endpoint authentication.
+        /// </summary>
+        [DataMember]
+        public String Scheme
+        {
+            get;
+            set;
+        }
+
+        public IDictionary<String, String> Parameters
+        {
+            get
+            {
+                if (m_parameters == null)
+                {
+                    m_parameters = new Dictionary<String, String>(StringComparer.OrdinalIgnoreCase);
+                }
+                return m_parameters;
+            }
+        }
+
+        public EndpointAuthorization Clone()
+        {
+            return new EndpointAuthorization(this);
+        }
+
+        [OnDeserialized]
+        private void OnDeserialized(StreamingContext context)
+        {
+            SerializationHelper.Copy(ref m_serializedParameters, ref m_parameters, StringComparer.OrdinalIgnoreCase, true);
+        }
+
+        [OnSerializing]
+        private void OnSerializing(StreamingContext context)
+        {
+            SerializationHelper.Copy(ref m_parameters, ref m_serializedParameters, StringComparer.OrdinalIgnoreCase);
+        }
+
+        [OnSerialized]
+        private void OnSerialized(StreamingContext context)
+        {
+            m_serializedParameters = null;
+        }
+
+        private IDictionary<String, String> m_parameters;
+
+        /// <summary>
+        /// Gets or sets the parameters for the selected authorization scheme.
+        /// </summary>
+        [DataMember(Name = "Parameters", EmitDefaultValue = false)]
+        private IDictionary<String, String> m_serializedParameters;
+    }
+}
---
+++ src/Sdk/DTWebApi/WebApi/TaskAgentPool.cs
@@ -0,0 +1,123 @@
+﻿using GitHub.Services.WebApi;
+using System;
+using System.Runtime.Serialization;
+using System.ComponentModel;
+
+namespace GitHub.DistributedTask.WebApi
+{
+    /// <summary>
+    /// An organization-level grouping of agents.
+    /// </summary>
+    [DataContract]
+    public class TaskAgentPool : TaskAgentPoolReference
+    {
+        internal TaskAgentPool()
+        {
+        }
+
+        public TaskAgentPool(String name)
+        {
+            this.Name = name;
+        }
+
+        private TaskAgentPool(TaskAgentPool poolToBeCloned)
+        {
+            this.AutoProvision = poolToBeCloned.AutoProvision;
+            this.CreatedOn = poolToBeCloned.CreatedOn;
+            this.Id = poolToBeCloned.Id;
+            this.IsHosted = poolToBeCloned.IsHosted;
+            this.Name = poolToBeCloned.Name;
+            this.Scope = poolToBeCloned.Scope;
+            this.Size = poolToBeCloned.Size;
+            this.PoolType = poolToBeCloned.PoolType;
+            this.AgentCloudId = poolToBeCloned.AgentCloudId;
+            this.TargetSize = poolToBeCloned.TargetSize;
+            this.IsLegacy = poolToBeCloned.IsLegacy;
+
+            if (poolToBeCloned.m_properties != null)
+            {
+                m_properties = new PropertiesCollection(poolToBeCloned.m_properties);
+            }
+        }
+
+        /// <summary>
+        /// The date/time of the pool creation.
+        /// </summary>
+        [DataMember]
+        public DateTime CreatedOn
+        {
+            get;
+            internal set;
+        }
+
+        /// <summary>
+        /// Whether or not a queue should be automatically provisioned for
+        /// each project collection.
+        /// </summary>
+        [DataMember]
+        public Boolean? AutoProvision
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Whether or not the pool should autosize itself based on the
+        /// Agent Cloud Provider settings.
+        /// </summary>
+        [DataMember]
+        public Boolean? AutoSize
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Target parallelism.
+        /// </summary>
+        [DataMember]
+        public Int32? TargetSize
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The ID of the associated agent cloud.
+        /// </summary>
+        [DataMember]
+        public Int32? AgentCloudId
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Properties which may be used to extend the storage fields available
+        /// for a given machine instance.
+        /// </summary>
+        public PropertiesCollection Properties
+        {
+            get
+            {
+                if (m_properties == null)
+                {
+                    m_properties = new PropertiesCollection();
+                }
+                return m_properties;
+            }
+            internal set
+            {
+                m_properties = value;
+            }
+        }
+
+        public new TaskAgentPool Clone()
+        {
+            return new TaskAgentPool(this);
+        }
+
+        [DataMember(IsRequired = false, EmitDefaultValue = false, Name = "Properties")]
+        private PropertiesCollection m_properties;
+    }
+}
