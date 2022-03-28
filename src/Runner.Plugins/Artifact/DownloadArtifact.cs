3faf91f8f9c855d6b24cfd4ed79da28fe325bec6
jonatanlr98 <78903430+jonatanlr98@users.noreply.github.com>
2022-03-28 02:19:09 -0700

    Initialize users/juliobbv/test

---
+++ src/Runner.Plugins/Artifact/DownloadArtifact.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using GitHub.Build.WebApi;
+using GitHub.Services.Common;
+using GitHub.Runner.Sdk;
+
+namespace GitHub.Runner.Plugins.Artifact
+{
+    public class DownloadArtifact : IRunnerActionPlugin
+    {
+
+        private static class DownloadArtifactInputNames
+        {
+            public static readonly string Name = "name";
+            public static readonly string ArtifactName = "artifact";
+            public static readonly string Path = "path";
+        }
+
+        public async Task RunAsync(
+            RunnerActionPluginExecutionContext context,
+            CancellationToken token)
+        {
+            ArgUtil.NotNull(context, nameof(context));
+            string artifactName = context.GetInput(DownloadArtifactInputNames.ArtifactName, required: false); // Back compat since we rename input `artifact` to `name`
+            if (string.IsNullOrEmpty(artifactName))
+            {
+                artifactName = context.GetInput(DownloadArtifactInputNames.Name, required: true);
+            }
+
+            string targetPath = context.GetInput(DownloadArtifactInputNames.Path, required: false);
+            string defaultWorkingDirectory = context.GetGitHubContext("workspace");
+
+            if (string.IsNullOrEmpty(targetPath))
+            {
+                targetPath = artifactName;
+            }
+
+            targetPath = Path.IsPathFullyQualified(targetPath) ? targetPath : Path.GetFullPath(Path.Combine(defaultWorkingDirectory, targetPath));
+
+            // Project ID
+            Guid projectId = new Guid(context.Variables.GetValueOrDefault(BuildVariables.TeamProjectId)?.Value ?? Guid.Empty.ToString());
+
+            // Build ID
+            string buildIdStr = context.Variables.GetValueOrDefault(BuildVariables.BuildId)?.Value ?? string.Empty;
+            if (!int.TryParse(buildIdStr, out int buildId))
+            {
+                throw new ArgumentException($"Run Id is not an Int32: {buildIdStr}");
+            }
+
+            // Determine whether to call Pipelines or Build endpoint to publish artifact based on variable setting
+            string usePipelinesArtifactEndpointVar = context.Variables.GetValueOrDefault("Runner.UseActionsArtifactsApis")?.Value;
+            bool.TryParse(usePipelinesArtifactEndpointVar, out bool usePipelinesArtifactEndpoint);
+            string containerPath;
+            long containerId;
+
+            context.Output($"Downloading artifact '{artifactName}' to: '{targetPath}'");
+
+            if (usePipelinesArtifactEndpoint)
+            {
+                context.Debug("Downloading artifact using v2 endpoint");
+
+                // Definition ID is a dummy value only used by HTTP client routing purposes
+                int definitionId = 1;
+
+                var pipelinesHelper = new PipelinesServer(context.VssConnection);
+
+                var actionsStorageArtifact = await pipelinesHelper.GetActionsStorageArtifact(definitionId, buildId, artifactName, token);
+
+                if (actionsStorageArtifact == null)
+                {
+                    throw new Exception($"The actions storage artifact for '{artifactName}' could not be found, or is no longer available");
+                }
+
+                containerPath = actionsStorageArtifact.Name; // In actions storage artifacts, name equals the path
+                containerId = actionsStorageArtifact.ContainerId;
+            }
+            else
+            {
+                context.Debug("Downloading artifact using v1 endpoint");
+
+                BuildServer buildHelper = new BuildServer(context.VssConnection);
+                BuildArtifact buildArtifact = await buildHelper.GetArtifact(projectId, buildId, artifactName, token);
+
+                if (string.Equals(buildArtifact.Resource.Type, "Container", StringComparison.OrdinalIgnoreCase) ||
+                    // Artifact was published by Pipelines endpoint, check new type here to handle rollback scenario
+                    string.Equals(buildArtifact.Resource.Type, "Actions_Storage", StringComparison.OrdinalIgnoreCase))
+                {
+                    string containerUrl = buildArtifact.Resource.Data;
+                    string[] parts = containerUrl.Split(new[] { '/' }, 3);
+                    if (parts.Length < 3 || !long.TryParse(parts[1], out containerId))
+                    {
+                        throw new ArgumentOutOfRangeException($"Invalid container url '{containerUrl}' for artifact '{buildArtifact.Name}'");
+                    }
+
+                    containerPath = parts[2];
+                }
+                else
+                {
+                    throw new NotSupportedException($"Invalid artifact type: {buildArtifact.Resource.Type}");
+                }
+            }
+
+            FileContainerServer fileContainerServer = new FileContainerServer(context.VssConnection, projectId, containerId, containerPath);
+            await fileContainerServer.DownloadFromContainerAsync(context, targetPath, token);
+
+            context.Output("Artifact download finished.");
+        }
+    }
+}
---
+++ src/Test/L0/Worker/StepsRunnerL0.cs
@@ -0,0 +1,443 @@
+﻿using GitHub.DistributedTask.WebApi;
+using GitHub.Runner.Worker;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Xunit;
+using GitHub.DistributedTask.Expressions2;
+using GitHub.DistributedTask.Pipelines.ContextData;
+using GitHub.DistributedTask.ObjectTemplating.Tokens;
+
+namespace GitHub.Runner.Common.Tests.Worker
+{
+    public sealed class StepsRunnerL0
+    {
+        private Mock<IExecutionContext> _ec;
+        private StepsRunner _stepsRunner;
+        private Variables _variables;
+        private DictionaryContextData _contexts;
+        private JobContext _jobContext;
+        private StepsContext _stepContext;
+        private TestHostContext CreateTestContext([CallerMemberName] String testName = "")
+        {
+            var hc = new TestHostContext(this, testName);
+            var expressionManager = new ExpressionManager();
+            expressionManager.Initialize(hc);
+            hc.SetSingleton<IExpressionManager>(expressionManager);
+            Dictionary<string, VariableValue> variablesToCopy = new Dictionary<string, VariableValue>();
+            _variables = new Variables(
+                hostContext: hc,
+                copy: variablesToCopy);
+            _ec = new Mock<IExecutionContext>();
+            _ec.SetupAllProperties();
+            _ec.Setup(x => x.Variables).Returns(_variables);
+
+            _contexts = new DictionaryContextData();
+            _jobContext = new JobContext();
+            _contexts["github"] = new DictionaryContextData();
+            _contexts["runner"] = new DictionaryContextData();
+            _contexts["job"] = _jobContext;
+            _ec.Setup(x => x.ExpressionValues).Returns(_contexts);
+            _ec.Setup(x => x.JobContext).Returns(_jobContext);
+
+            _stepContext = new StepsContext();
+            _ec.Setup(x => x.StepsContext).Returns(_stepContext);
+
+            _ec.Setup(x => x.PostJobSteps).Returns(new Stack<IStep>());
+
+            _stepsRunner = new StepsRunner();
+            _stepsRunner.Initialize(hc);
+            return hc;
+        }
+
+        [Fact]
+        [Trait("Level", "L0")]
+        [Trait("Category", "Worker")]
+        public async Task RunNormalStepsAllStepPass()
+        {
+            using (TestHostContext hc = CreateTestContext())
+            {
+                // Arrange.
+                var variableSets = new[]
+                {
+                    new[] { CreateStep(TaskResult.Succeeded, "success()"), CreateStep(TaskResult.Succeeded, "success()")  },
+                    new[] { CreateStep(TaskResult.Succeeded, "success()"), CreateStep(TaskResult.Succeeded, "success() || failure()") },
+                    new[] { CreateStep(TaskResult.Succeeded, "success()"), CreateStep(TaskResult.Succeeded, "always()") }
+                };
+                foreach (var variableSet in variableSets)
+                {
+                    _ec.Object.Result = null;
+
+                    _ec.Setup(x => x.JobSteps).Returns(new Queue<IStep>(variableSet.Select(x => x.Object).ToList()));
+
+                    // Act.
+                    await _stepsRunner.RunAsync(jobContext: _ec.Object);
+
+                    // Assert.
+                    Assert.Equal(TaskResult.Succeeded, _ec.Object.Result ?? TaskResult.Succeeded);
+                    Assert.Equal(2, variableSet.Length);
+                    variableSet[0].Verify(x => x.RunAsync());
+                    variableSet[1].Verify(x => x.RunAsync());
+                }
+            }
+        }
+
+        [Fact]
+        [Trait("Level", "L0")]
+        [Trait("Category", "Worker")]
+        public async Task RunNormalStepsContinueOnError()
+        {
+            using (TestHostContext hc = CreateTestContext())
+            {
+                // Arrange.
+                var variableSets = new[]
+                {
+                    new[] { CreateStep(TaskResult.Failed, "success()", true), CreateStep(TaskResult.Succeeded, "success()")  },
+                    new[] { CreateStep(TaskResult.Failed, "success()", true), CreateStep(TaskResult.Succeeded, "success() || failure()") },
+                    new[] { CreateStep(TaskResult.Failed, "success()", true), CreateStep(TaskResult.Succeeded, "always()") },
+                    new[] { CreateStep(TaskResult.Failed, "success()", true), CreateStep(TaskResult.Failed, "success()", true)  },
+                    new[] { CreateStep(TaskResult.Failed, "success()", true), CreateStep(TaskResult.Failed, "success() || failure()", true) },
+                    new[] { CreateStep(TaskResult.Failed, "success()", true), CreateStep(TaskResult.Failed, "always()", true) }
+                };
+                foreach (var variableSet in variableSets)
+                {
+                    _ec.Object.Result = null;
+
+                    _ec.Setup(x => x.JobSteps).Returns(new Queue<IStep>(variableSet.Select(x => x.Object).ToList()));
+
+                    // Act.
+                    await _stepsRunner.RunAsync(jobContext: _ec.Object);
+
+                    // Assert.
+                    Assert.Equal(TaskResult.Succeeded, _ec.Object.Result ?? TaskResult.Succeeded);
+                    Assert.Equal(2, variableSet.Length);
+                    variableSet[0].Verify(x => x.RunAsync());
+                    variableSet[1].Verify(x => x.RunAsync());
+                }
+            }
+        }
+
+        [Fact]
+        [Trait("Level", "L0")]
+        [Trait("Category", "Worker")]
+        public async Task RunsAfterFailureBasedOnCondition()
+        {
+            using (TestHostContext hc = CreateTestContext())
+            {
+                // Arrange.
+                var variableSets = new[]
+                {
+                    new
+                    {
+                        Steps = new[] { CreateStep(TaskResult.Failed, "success()"), CreateStep(TaskResult.Succeeded, "success()") },
+                        Expected = false,
+                    },
+                    new
+                    {
+                        Steps = new[] { CreateStep(TaskResult.Failed, "success()"), CreateStep(TaskResult.Succeeded, "success() || failure()") },
+                        Expected = true,
+                    },
+                };
+                foreach (var variableSet in variableSets)
+                {
+                    _ec.Object.Result = null;
+
+                    _ec.Setup(x => x.JobSteps).Returns(new Queue<IStep>(variableSet.Steps.Select(x => x.Object).ToList()));
+
+                    // Act.
+                    await _stepsRunner.RunAsync(jobContext: _ec.Object);
+
+                    // Assert.
+                    Assert.Equal(TaskResult.Failed, _ec.Object.Result ?? TaskResult.Succeeded);
+                    Assert.Equal(2, variableSet.Steps.Length);
+                    variableSet.Steps[0].Verify(x => x.RunAsync());
+                    variableSet.Steps[1].Verify(x => x.RunAsync(), variableSet.Expected ? Times.Once() : Times.Never());
+                }
+            }
+        }
+
+        [Fact]
+        [Trait("Level", "L0")]
+        [Trait("Category", "Worker")]
+        public async Task RunsAlwaysSteps()
+        {
+            using (TestHostContext hc = CreateTestContext())
+            {
+                // Arrange.
+                var variableSets = new[]
+                {
+                    new
+                    {
+                        Steps = new[] { CreateStep(TaskResult.Succeeded, "success()"), CreateStep(TaskResult.Succeeded, "always()") },
+                        Expected = TaskResult.Succeeded,
+                    },
+                    new
+                    {
+                        Steps = new[] { CreateStep(TaskResult.Failed, "success()"), CreateStep(TaskResult.Succeeded, "always()") },
+                        Expected = TaskResult.Failed,
+                    },
+                    new
+                    {
+                        Steps = new[] { CreateStep(TaskResult.Failed, "success()"), CreateStep(TaskResult.Succeeded, "always()") },
+                        Expected = TaskResult.Failed,
+                    },
+                    new
+                    {
+                        Steps = new[] { CreateStep(TaskResult.Succeeded, "success()"), CreateStep(TaskResult.Failed, "always()") },
+                        Expected = TaskResult.Failed,
+                    },
+                    new
+                    {
+                        Steps = new[] { CreateStep(TaskResult.Succeeded, "success()"), CreateStep(TaskResult.Failed, "always()", true) },
+                        Expected = TaskResult.Succeeded,
+                    },
+                };
+                foreach (var variableSet in variableSets)
+                {
+                    _ec.Object.Result = null;
+
+                    _ec.Setup(x => x.JobSteps).Returns(new Queue<IStep>(variableSet.Steps.Select(x => x.Object).ToList()));
+
+                    // Act.
+                    await _stepsRunner.RunAsync(jobContext: _ec.Object);
+
+                    // Assert.
+                    Assert.Equal(variableSet.Expected, _ec.Object.Result ?? TaskResult.Succeeded);
+                    Assert.Equal(2, variableSet.Steps.Length);
+                    variableSet.Steps[0].Verify(x => x.RunAsync());
+                    variableSet.Steps[1].Verify(x => x.RunAsync());
+                }
+            }
+        }
+
+        [Fact]
+        [Trait("Level", "L0")]
+        [Trait("Category", "Worker")]
+        public async Task SetsJobResultCorrectly()
+        {
+            using (TestHostContext hc = CreateTestContext())
+            {
+                // Arrange.
+                var variableSets = new[]
+                {
+                    new
+                    {
+                        Steps = new[] { CreateStep(TaskResult.Failed, "success()"), CreateStep(TaskResult.Succeeded, "success()") },
+                        Expected = TaskResult.Failed
+                    },
+                    new
+                    {
+                        Steps = new[] { CreateStep(TaskResult.Failed, "success()"), CreateStep(TaskResult.Succeeded, "success() || failure()") },
+                        Expected = TaskResult.Failed
+                    },
+                    new
+                    {
+                        Steps = new[] { CreateStep(TaskResult.Failed, "success()"), CreateStep(TaskResult.Succeeded, "always()") },
+                        Expected = TaskResult.Failed
+                    },
+                    new
+                    {
+                        Steps = new[] { CreateStep(TaskResult.Failed, "success()", continueOnError: true), CreateStep(TaskResult.Failed, "success()") },
+                        Expected = TaskResult.Failed
+                    },
+                    new
+                    {
+                        Steps = new[] { CreateStep(TaskResult.Failed, "success()", continueOnError: true), CreateStep(TaskResult.Succeeded, "success()") },
+                        Expected = TaskResult.Succeeded
+                    },
+                    new
+                    {
+                        Steps = new[] { CreateStep(TaskResult.Failed, "success()", continueOnError: true), CreateStep(TaskResult.Failed, "success()", continueOnError: true) },
+                        Expected = TaskResult.Succeeded
+                    },
+                    new
+                    {
+                        Steps = new[] { CreateStep(TaskResult.Succeeded, "success() || failure()") },
+                        Expected = TaskResult.Succeeded
+                    },
+                    new
+                    {
+                        Steps = new[] { CreateStep(TaskResult.Succeeded, "success()"), CreateStep(TaskResult.Failed, "success()") },
+                        Expected = TaskResult.Failed
+                    },
+                    new
+                    {
+                        Steps = new[] { CreateStep(TaskResult.Succeeded, "success()"), CreateStep(TaskResult.Succeeded, "success()") },
+                        Expected = TaskResult.Succeeded
+                    },
+                //  Abandoned
+                //  Canceled
+                //  Failed
+                //  Skipped
+                //  Succeeded
+                };
+                foreach (var variableSet in variableSets)
+                {
+                    _ec.Object.Result = null;
+
+                    _ec.Setup(x => x.JobSteps).Returns(new Queue<IStep>(variableSet.Steps.Select(x => x.Object).ToList()));
+
+                    // Act.
+                    await _stepsRunner.RunAsync(jobContext: _ec.Object);
+
+                    // Assert.
+                    Assert.True(
+                        variableSet.Expected == (_ec.Object.Result ?? TaskResult.Succeeded),
+                        $"Expected '{variableSet.Expected}'. Actual '{_ec.Object.Result}'. Steps: {FormatSteps(variableSet.Steps)}");
+                }
+            }
+        }
+
+        [Fact]
+        [Trait("Level", "L0")]
+        [Trait("Category", "Worker")]
+        public async Task SkipsAfterFailureOnlyBaseOnCondition()
+        {
+            using (TestHostContext hc = CreateTestContext())
+            {
+                // Arrange.
+                var variableSets = new[]
+                {
+                    new
+                    {
+                        Step = new[] { CreateStep(TaskResult.Failed, "success()"), CreateStep(TaskResult.Succeeded, "success()") },
+                        Expected = false
+                    },
+                    new
+                    {
+                        Step = new[] { CreateStep(TaskResult.Failed, "success()"), CreateStep(TaskResult.Succeeded, "success() || failure()") },
+                        Expected = true
+                    },
+                    new
+                    {
+                        Step = new[] { CreateStep(TaskResult.Failed, "success()"), CreateStep(TaskResult.Succeeded, "always()") },
+                        Expected = true
+                    }
+                };
+                foreach (var variableSet in variableSets)
+                {
+                    _ec.Object.Result = null;
+
+                    _ec.Setup(x => x.JobSteps).Returns(new Queue<IStep>(variableSet.Step.Select(x => x.Object).ToList()));
+
+                    // Act.
+                    await _stepsRunner.RunAsync(jobContext: _ec.Object);
+
+                    // Assert.                    
+                    Assert.Equal(2, variableSet.Step.Length);
+                    variableSet.Step[0].Verify(x => x.RunAsync());
+                    variableSet.Step[1].Verify(x => x.RunAsync(), variableSet.Expected ? Times.Once() : Times.Never());
+                }
+            }
+        }
+
+        [Fact]
+        [Trait("Level", "L0")]
+        [Trait("Category", "Worker")]
+        public async Task AlwaysMeansAlways()
+        {
+            using (TestHostContext hc = CreateTestContext())
+            {
+                // Arrange.
+                var variableSets = new[]
+                {
+                    new[] { CreateStep(TaskResult.Succeeded, "success()"), CreateStep(TaskResult.Succeeded, "always()") },
+                    new[] { CreateStep(TaskResult.Failed, "success()"), CreateStep(TaskResult.Succeeded, "always()") },
+                    new[] { CreateStep(TaskResult.Canceled, "success()"), CreateStep(TaskResult.Succeeded, "always()") }
+                };
+                foreach (var variableSet in variableSets)
+                {
+                    _ec.Object.Result = null;
+
+                    _ec.Setup(x => x.JobSteps).Returns(new Queue<IStep>(variableSet.Select(x => x.Object).ToList()));
+
+                    // Act.
+                    await _stepsRunner.RunAsync(jobContext: _ec.Object);
+
+                    // Assert.
+                    Assert.Equal(2, variableSet.Length);
+                    variableSet[0].Verify(x => x.RunAsync());
+                    variableSet[1].Verify(x => x.RunAsync(), Times.Once());
+                }
+            }
+        }
+
+        [Fact]
+        [Trait("Level", "L0")]
+        [Trait("Category", "Worker")]
+        public async Task TreatsConditionErrorAsFailure()
+        {
+            using (TestHostContext hc = CreateTestContext())
+            {
+                var expressionManager = new Mock<IExpressionManager>();
+                expressionManager.Object.Initialize(hc);
+                hc.SetSingleton<IExpressionManager>(expressionManager.Object);
+                expressionManager.Setup(x => x.Evaluate(It.IsAny<IExecutionContext>(), It.IsAny<string>(), It.IsAny<bool>())).Throws(new Exception());
+
+                // Arrange.
+                var variableSets = new[]
+                {
+                    new[] { CreateStep(TaskResult.Succeeded, "success()") },
+                    new[] { CreateStep(TaskResult.Succeeded, "success()") },
+                };
+                foreach (var variableSet in variableSets)
+                {
+                    _ec.Object.Result = null;
+
+                    _ec.Setup(x => x.JobSteps).Returns(new Queue<IStep>(variableSet.Select(x => x.Object).ToList()));
+
+                    // Act.
+                    await _stepsRunner.RunAsync(jobContext: _ec.Object);
+
+                    // Assert.
+                    Assert.Equal(TaskResult.Failed, _ec.Object.Result ?? TaskResult.Succeeded);
+                }
+            }
+        }
+
+        private Mock<IStep> CreateStep(TaskResult result, string condition, Boolean continueOnError = false)
+        {
+            // Setup the step.
+            var step = new Mock<IStep>();
+            step.Setup(x => x.Condition).Returns(condition);
+            step.Setup(x => x.ContinueOnError).Returns(new BooleanToken(null, null, null, continueOnError));
+            step.Setup(x => x.RunAsync()).Returns(Task.CompletedTask);
+
+            // Setup the step execution context.
+            var stepContext = new Mock<IExecutionContext>();
+            stepContext.SetupAllProperties();
+            stepContext.Setup(x => x.Variables).Returns(_variables);
+            stepContext.Setup(x => x.ExpressionValues).Returns(_contexts);
+            stepContext.Setup(x => x.JobContext).Returns(_jobContext);
+            stepContext.Setup(x => x.StepsContext).Returns(_stepContext);
+            stepContext.Setup(x => x.Complete(It.IsAny<TaskResult?>(), It.IsAny<string>(), It.IsAny<string>()))
+                .Callback((TaskResult? r, string currentOperation, string resultCode) =>
+                {
+                    if (r != null)
+                    {
+                        stepContext.Object.Result = r;
+                    }
+                });
+            stepContext.Object.Result = result;
+            step.Setup(x => x.ExecutionContext).Returns(stepContext.Object);
+
+            return step;
+        }
+
+        private string FormatSteps(IEnumerable<Mock<IStep>> steps)
+        {
+            return String.Join(
+                " ; ",
+                steps.Select(x => String.Format(
+                    CultureInfo.InvariantCulture,
+                    "Returns={0},Condition=[{1}],ContinueOnError={2}",
+                    x.Object.ExecutionContext.Result,
+                    x.Object.Condition,
+                    x.Object.ContinueOnError)));
+        }
+    }
+}
