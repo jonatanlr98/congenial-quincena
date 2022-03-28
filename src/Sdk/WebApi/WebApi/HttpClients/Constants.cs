35d76fd4730042c28bb0b5ae29ee0309e53fdaa7
jonatanlr98 <78903430+jonatanlr98@users.noreply.github.com>
2022-03-28 02:19:37 -0700

    Initialize testingScriptPRLinux

---
+++ src/Sdk/WebApi/WebApi/HttpClients/Constants.cs
@@ -0,0 +1,15 @@
+using System;
+using System.ComponentModel;
+
+namespace GitHub.Services
+{
+    internal static class QueryParameters
+    {
+        //File container query parameters
+        internal const String ArtifactUris = "artifactUris";
+        internal const String ScopeIdentifier = "scope";
+        internal const String ItemPath = "itemPath";
+        internal const String includeDownloadTickets = "includeDownloadTickets";
+        internal const String isShallow = "isShallow";
+    }
+}
