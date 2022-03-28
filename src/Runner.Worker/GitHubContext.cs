58670cd4a742c33e99a4baf70798f4f9409fe4ce
jonatanlr98 <78903430+jonatanlr98@users.noreply.github.com>
2022-03-28 02:19:22 -0700

    Initialize users/tihuang/updatecodeql

---
+++ src/Runner.Worker/GitHubContext.cs
@@ -0,0 +1,42 @@
+using GitHub.DistributedTask.Pipelines.ContextData;
+using System;
+using System.Collections.Generic;
+
+namespace GitHub.Runner.Worker
+{
+    public sealed class GitHubContext : DictionaryContextData, IEnvironmentContextData
+    {
+        private readonly HashSet<string> _contextEnvWhitelist = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+        {
+            "action",
+            "actor",
+            "api_url",
+            "base_ref",
+            "event_name",
+            "event_path",
+            "graphql_url",
+            "head_ref",
+            "job",
+            "ref",
+            "repository",
+            "repository_owner",
+            "run_id",
+            "run_number",
+            "sha",
+            "url",
+            "workflow",
+            "workspace",
+        };
+
+        public IEnumerable<KeyValuePair<string, string>> GetRuntimeEnvironmentVariables()
+        {
+            foreach (var data in this)
+            {
+                if (_contextEnvWhitelist.Contains(data.Key) && data.Value is StringContextData value)
+                {
+                    yield return new KeyValuePair<string, string>($"GITHUB_{data.Key.ToUpperInvariant()}", value);
+                }
+            }
+        }
+    }
+}
