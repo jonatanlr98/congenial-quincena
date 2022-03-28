f1ade24a65c70fc79ac0783cb2420abe64695b1f
jonatanlr98 <78903430+jonatanlr98@users.noreply.github.com>
2022-03-28 02:19:11 -0700

    Initialize users/juliobbv/checkWinSvcStarts

---
+++ src/Sdk/BuildWebApi/Api/ArtifactResourceTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using GitHub.Services.Common;
+
+namespace GitHub.Build.WebApi
+{
+    public static class ArtifactResourceTypes
+    {
+        /// <summary>
+        /// Build container reference
+        /// E.g. #/2121/drop
+        /// </summary>
+        public const String Container = "Container";
+    }
+}
