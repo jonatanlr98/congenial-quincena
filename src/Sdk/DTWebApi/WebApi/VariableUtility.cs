36d13f59d35e0e3127720665d3288444d988e8ec
jonatanlr98 <78903430+jonatanlr98@users.noreply.github.com>
2022-03-28 02:19:25 -0700

    Initialize releases/m263

---
+++ src/Sdk/DTWebApi/WebApi/VariableUtility.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using GitHub.DistributedTask.Pipelines;
+using GitHub.Services.WebApi;
+using Newtonsoft.Json.Linq;
+
+namespace GitHub.DistributedTask.WebApi
+{
+    public static class VariableUtility
+    {
+        public static VariableValue Clone(this VariableValue value)
+        {
+            return new VariableValue(value);
+        }
+    }
+}
