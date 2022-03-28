5343452160f3b6b446d269d488d24179949df26a
jonatanlr98 <78903430+jonatanlr98@users.noreply.github.com>
2022-03-28 02:19:09 -0700

    Initialize users/juliobbv/bringBackFallback

---
+++ src/Sdk/BuildWebApi/Api/BuildVariables.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GitHub.Build.WebApi
+{
+    public static class BuildVariables
+    {
+        public const String TeamProjectId = "system.teamProjectId";
+
+        public const String BuildId = "build.buildId";
+        public const String BuildNumber = "build.buildNumber";
+        public const String ContainerId = "build.containerId";
+    }
+}
